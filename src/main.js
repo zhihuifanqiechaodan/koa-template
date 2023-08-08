e3a88b6b3f9b51e4ec61a83134ddec2491994d50
高宇 <673395239@qq.com>
2023-08-08 15:01:34 +0800

    feat: init

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './env.js';
+
+import path from 'path';
+import Koa from 'koa';
+import { koaBody } from 'koa-body';
+import koa2Cors from 'koa2-cors';
+import koaStatic from 'koa-static';
+import koaHelmet from 'koa-helmet';
+
+import errHandler from './middleware/err-handler.js';
+import router from './router/index.js';
+import { log4jsInfo } from './utils/lo4js.js';
+
+const app = new Koa();
+
+app
+  .use(koaHelmet())
+  .use(koa2Cors())
+  .use(koaBody({ multipart: true }))
+  .use(koaStatic(path.resolve(process.cwd(), 'src/assets')))
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .on('error', errHandler)
+  .listen(process.env.APP_PORT, () => log4jsInfo(`server is running on http://localhost:${process.env.APP_PORT}`));
