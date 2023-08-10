7ec52a2a5493c39cc3c9a02219093667a0ecfe74
高宇 <673395239@qq.com>
2023-08-10 11:50:55 +0800

    feat: 新增node版本显示，格式化

--- src/utils/lo4js.js
+++ src/utils/lo4js.js
@@ -1,9 +1,9 @@
-import log4js from "log4js";
-import path from "path";
+import log4js from 'log4js';
+import path from 'path';
 
-const basePath = path.join(process.cwd(), "src/logs");
+const basePath = path.join(process.cwd(), 'src/logs');
 
-log4js.addLayout("api", function () {
+log4js.addLayout('api', function () {
   return function (logEvent) {
     if (logEvent.level.isGreaterThanOrEqualTo(log4js.levels.ERROR)) {
       (async () => {
@@ -11,40 +11,42 @@ log4js.addLayout("api", function () {
       })();
     }
 
-    return "";
+    return '';
   };
 });
 
 log4js.configure({
   appenders: {
-    console: { type: "console" },
+    console: { type: 'console' },
     error: {
-      type: "dateFile",
+      type: 'dateFile',
       filename: `${basePath}/errors/error`,
-      pattern: "-yyyy-MM-dd.log",
+      pattern: '-yyyy-MM-dd.log',
       alwaysIncludePattern: true,
       layout: {
-        type: "pattern",
-        pattern: "%d{yyyy-MM-dd hh:mm:ss.SSS} [%p] %c - %m",
+        type: 'pattern',
+        pattern: '%d{yyyy-MM-dd hh:mm:ss.SSS} [%p] %c - %m',
       },
+      compress: true,
     },
     info: {
-      type: "dateFile",
+      type: 'dateFile',
       filename: `${basePath}/infos/info`,
-      pattern: "-yyyy-MM-dd.log",
+      pattern: '-yyyy-MM-dd.log',
       alwaysIncludePattern: true,
       layout: {
-        type: "pattern",
-        pattern: "%d{yyyy-MM-dd hh:mm:ss.SSS} [%p] %c - %m",
+        type: 'pattern',
+        pattern: '%d{yyyy-MM-dd hh:mm:ss.SSS} [%p] %c - %m',
       },
+      compress: true,
     },
     // custom layout
-    api: { type: "console", layout: { type: "api" } },
+    api: { type: 'console', layout: { type: 'api' } },
   },
   categories: {
-    default: { appenders: ["console"], level: "info" },
-    error: { appenders: ["error", "api"], level: "error" },
-    info: { appenders: ["info"], level: "info" },
+    default: { appenders: ['console'], level: 'info' },
+    error: { appenders: ['error', 'api'], level: 'error' },
+    info: { appenders: ['info'], level: 'info' },
   },
 });
 
@@ -53,13 +55,13 @@ log4js.configure({
  * @param {string} message
  * @returns
  */
-const log4jsInfo = (message) => log4js.getLogger("info").info(message);
+const log4jsInfo = (message) => log4js.getLogger('info').info(message);
 
 /**
  * @method log4jsError
  * @param {string} message
  * @returns
  */
-const log4jsError = (message) => log4js.getLogger("error").error(message);
+const log4jsError = (message) => log4js.getLogger('error').error(message);
 
 export { log4jsError, log4jsInfo };
